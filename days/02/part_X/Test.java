ee05c09987e7a79d5a4022176a32b6bad4073e04
frogfrequency <braun.marcel@hotmail.com>
2023-03-09 11:26:47 +0100

    add puzzle template

--- days/02/part_X/Test.java
+++
@@ -1,4 +0,0 @@
-public class Test {
-    public String[] testtest = {"123", "123,", "2342jsdfjsdlfk"};
-}
-
---
+++ days/XX/part_1/PartX.java
@@ -0,0 +1,6 @@
+
+public class PartX {
+    public static void main(String[] args) {
+
+    }
+}
